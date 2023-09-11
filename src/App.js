bf60e089b62a02226909cb56b84d71b16823077c
Eduardo Tornquist <eatornquist04@gmail.com>
2023-09-11 14:54:20 -0400

    First and last commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
-  );
+  )
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1)
+  const [count, setCount] = useState(0)
+
+  const date = new Date()
+  date.setDate(date.getDate() + count)
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setStep((s) => s - 1)}> - </button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((s) => s + 1)}> + </button>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}> - </button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}> + </button>
+      </div>
+
+      <div>
+        <h1>
+          {count} days from today is {date.toDateString()}
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
